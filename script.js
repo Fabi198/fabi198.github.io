0ec7e8367797832dd58dc0b0f10a24d64cc736cc
Fabian Esteban Ferrer <132310263+Fabi198@users.noreply.github.com>
2026-04-06 00:21:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+<script>
+  function copyEmail() {
+    const email = "fabianferrer42@gmail.com";
+    navigator.clipboard.writeText(email);
 
+    const snackbar = document.getElementById("snackbar");
+    snackbar.classList.add("show");
+
+    setTimeout(() => {
+      snackbar.classList.remove("show");
+    }, 2000);
+  }
+
+  function showEmail() {
+    document.getElementById("emailBox").style.opacity = "1";
+  }
+
+  function hideEmail() {
+    document.getElementById("emailBox").style.opacity = "0";
+  }
+</script>
